a344d788e6bd3a0d09e8269249b8ce1134e1aabe
Hanady <Hanady_morshed@hotmail.com>
2019-07-14 10:18:32 +0300

    commit1

---
+++ exc.ts
@@ -0,0 +1,57 @@
+//npm install -g ts-node
+//npm install --save event-stream
+interface Mmemory{
+
+
+}
+
+class Excersuxe1{
+    private fs = require('fs')
+    private es = require('event-stream');
+    private lineNumber : number = 0;
+    private data:any[]=[];
+
+    constructor() { }
+
+
+    private LaodStopWords(){
+        this.fs.readFile('Text Folder\\stopwords.txt', (err: any, data: any) => {
+            if (err) {
+                return console.error(err);
+            }
+            this.data[0]= data.toString().split('\n');
+            //console.log(this.data[0][5])
+        });
+
+    }
+
+    ReadLineByLine(){
+
+        var s = this.fs.createReadStream('Text Folder\\dummy.txt')
+        .pipe(this.es.split())
+        .pipe(this.es.mapSync((line:string)=>{
+    
+            // pause the readstream
+            s.pause();
+    
+            this.lineNumber += 1;
+            console.log(line);
+    
+    
+    
+            // resume the readstream, possibly from a callback
+            s.resume();
+        })
+        .on('error', function(err: any){
+            console.log('Error while reading file.', err);
+        })
+        .on('end', function(){
+            console.log('Read entire file.')
+        })
+    );
+    }
+
+
+}
+let test: Excersuxe1 = new Excersuxe1();
+test.LaodStopWords();
