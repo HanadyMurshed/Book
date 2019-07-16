ecb13b148d522eb3cc119cb67e8402e8bc4ae7cd
Hanady <Hanady_morshed@hotmail.com>
2019-07-16 09:48:18 +0300

    count left to go

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 //npm install -g ts-node
 //npm install --save event-stream
+//fs.createReadStream() search for this approuche
 
 
 class Excersuxe1{
@@ -21,27 +22,47 @@ class Excersuxe1{
         });
 
     }
-    private ProcessLine(line : string ){
 
 
+
+    static isLetter(str:string) {
+        return str.length === 1 && str.match(/[a-z]/i);
     }
 
     ReadLineByLine(){
 
+        this.LaodStopWords();
         var s = this.fs.createReadStream('Text Folder\\dummy.txt')
         .pipe(this.es.split())
-        .pipe(this.es.mapSync((line:string)=>{
+        .pipe(this.es.mapSync((line:string)=>{ //line is a problem 
     
             // pause the readstream
             s.pause();
-    
-            this.lineNumber += 1;
-            if(line != '')
-                this.ProcessLine(line);
 
-    
-    
-    
+
+            this.data[1]=line;
+
+            this.lineNumber += 1;//for now not needed 
+
+            //start line prossceing 
+            if(this.data[1] == '') 
+             {return ;}
+            if (this.data[1][(this.data[1].length)-1] != '\n')
+               {this.data[1]+= '\n'}
+            this.data[2] = undefined;
+            this.data[3] = 0;
+
+            for( var c in this.data[1]){
+                if (this.data[2] == undefined){
+                    if (Excersuxe1.isLetter(c))// We found the start of a word
+                        this.data[2] = this.data[3];
+                    }
+                else{
+                    if (!Excersuxe1.isLetter(c))
+                        this.data[1] = this.data[1] + '\n';
+                    }
+                }
+
             // resume the readstream, possibly from a callback
             s.resume();
         })
@@ -53,7 +74,8 @@ class Excersuxe1{
         })
     );
     }
-
-
+  
 }
+
+
 let test: Excersuxe1 = new Excersuxe1();
