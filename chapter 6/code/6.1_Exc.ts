5058ac468d13845773085328b4139aabd9e88bea
Hanady <Hanady_morshed@hotmail.com>
2019-07-28 14:11:10 +0300

    link to tge collection js package decumentatiom

--- chapter 6/code/6.1_Exc.ts
+++ chapter 6/code/6.1_Exc.ts
@@ -1,3 +1,4 @@
 import { write, readFile } from "fs";
 let words = require('fs').readFileAsync('./input\\dummy.txt').toString().match(/[\W_|]+/g).toLowerCase();
-let stops =  require('fs').readFileAsync('./input\\dummy.txt').toString().split('\n')
+let stops =  require('fs').readFileAsync('./input\\stopwordss.txt').toString().split('\n')
+//https://www.collectionsjs.com/method/filter
