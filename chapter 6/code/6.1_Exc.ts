3efad39b42e34855235cde12f631129b23954f6c
Hanady <Hanady_morshed@hotmail.com>
2019-07-29 09:32:08 +0300

    6.1

--- chapter 6/code/6.1_Exc.ts
+++ chapter 6/code/6.1_Exc.ts
@@ -1,4 +1,9 @@
 import { write, readFile } from "fs";
-let words = require('fs').readFileAsync('./input\\dummy.txt').toString().match(/[\W_|]+/g).toLowerCase();
-let stops =  require('fs').readFileAsync('./input\\stopwordss.txt').toString().split('\n')
-//https://www.collectionsjs.com/method/filter
+let stops = require('fs').readFileSync('./input\\stopwords.txt').toString().split('\n')
+let words = require('fs').readFileSync('./input\\dummy.txt').toString().replace(/[\W_|]+/gi, " ").split(" ").filter(e => !(stops.includes(e)));
+let count = words.reduce((total, word) => { (word in total) ? total[word] += 1 : total[word] = 1; return total }, {})
+let sorted = Object.keys(count).map(function (key) { return [key, count[key]]; }).sort((first, second) => {
+    return second[1] - first[1];
+}).slice(0, 25);
+
+console.log(sorted)
